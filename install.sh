144f8e1b962bebc6c526026309ee14b0342325ab
Mr_MSDV <37757663+mrmsdv@users.noreply.github.com>
2018-08-31 20:52:25 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+echo "Please wait...."
+apt install unzip
+pkg install unzip
+pip2 install requests 
+
+unzip bin.zip
+echo "Success Install..."
+echo "Silahkan Tekan python2 SF.py"
+
+
